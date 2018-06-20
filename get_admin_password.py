f43b79dc4bfddb7561b455431b3329c019d4f6f5
Bryan Fisher <brf2010@med.cornell.edu>
2018-06-20 16:21:48 -0400

    added second authentication api request

--- get_admin_password.py
+++ get_admin_password.py
@@ -20,6 +20,7 @@ admin_username = default['admin_username']
 domain = default['domain']
 server = default['server']
 api_key = default['api_key']
+verify_ssl = default['verify_ssl']
 #for key in default: print(key, default[key])
 
 # post-processing for config
@@ -44,8 +45,19 @@ bt_session = requests.Session()
 
 # build request for initial auth
 initial_auth_url = server + bt_api['SignAppin']
-initial_auth_headers = {'Authorization': "PS-Auth key=" + api_key + "; runas=" + bt_user + "; pwd=[" + bt_user_password + "]"}
+initial_auth_headers = {'Authorization': "PS-Auth key=" + api_key + "; runas=" + bt_user + ";""}
 
 # make initial auth request
-response = bt_session.post(initial_auth_url, headers=initial_auth_headers)
-pprint(response)
+response = bt_session.post(initial_auth_url, headers=initial_auth_headers, verify=verify_ssl)
+
+# make second auth request with password and duo push information
+second_auth_url = server + bt_api['SignAppin']
+second_auth_headers = {'Authorization': "PS-Auth key=" + api_key + "; runas=" + bt_user + "; challenge=push; pwd=[" + bt_user_password + "]"}
+
+print("Please check your phone for a Duo authentication request")
+
+response = bt_session.post(second_auth_url, headers=second_auth_headers, verify=verify_ssl)
+
+pprint(response.status_code)
+pprint(response.headers)
+pprint(response.text)
